77f2c957a64576af582a06ffca274866741259f3
Bruno H. Homem <brunohhomem@gmail.com>
2025-01-11 14:21:59 -0300

    feat: added tailwindcss

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './app'
 
+//Componentes e propriedades
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
